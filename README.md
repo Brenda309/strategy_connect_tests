f22098f522ff8171389db371469e7dc806a25b6d
Brenda309 <brewihogora23@gmail.com>
2023-05-26 15:34:52 +0200

    Add the service page test

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# strategy_connect_tests
-Automation test for Strategy Connect website
+  # Strategy Connect Website Auto Tests using Cypress
+
+
+Automate the **Strategy Connect** website (https://www.strategyconnect.co).
+
+
+
+## Setup
+Cloned into a local directory:
+1. Open the terminal/command prompt and go to the cloned repo
+2. Install all dependancies by running the command `npm install`
+Note: before running the project you need to have node.js installed.
+## How to run
+|ID|Description| Command |
+| :---: | :--- | :--- |
+|1|Run all tests in headless mode|`npm test` or `npx cypress run`|
+|2|Run all tests in headed mode|`npx cypress open`|
+|3|Generate allure report|`npm run allure:report` or `allure generate ./allure-results --clean`|
+|4|Open report|`npm run openReport` or `allure open ./allure-report`|
+
+  
+## Tests coverage
+|ID|Description| Command |
+| :---: | :--- | :--- |
+|1|Service Packages |1. Navigate to `https://www.strategyconnect.co/` page<br>2. Verify if Home page has been opened<br>3. Click the "KnowMore" button on "Technology Due Diligence" section on Home page<br>4. Verify if the Strategy Service page got launched<br>5. Click the "KnowMore" button on "Technology Due Diligence" section on Strategy Service<br>6Verify if the Technical Due Diligence Page has been launched.<br>
---
+++ cypress/e2e/tests/servicePackages.cy.js
@@ -0,0 +1,51 @@
+/// <reference types="cypress" />
+
+import data from "../../fixtures/data.json";
+
+import {NavigateToURL} from '../../pages/1-navigateToURL.page'
+import {HomePage} from '../../pages/2-home.page'
+import { StrategyServicePage } from "../../pages/3-strategyServices.page";
+import { HeaderSection } from '../../pages/4-headerSection';
+import { TechnicalDueDiligencePage } from "../../pages/5-technicalDueDiligence.page";
+
+describe('Service Packages', () => {
+    const navigate = new NavigateToURL();
+    const homepage = new HomePage();
+    const headerSection = new HeaderSection();
+    const strategyService = new StrategyServicePage();
+    const technicalDueDiligence = new TechnicalDueDiligencePage();
+
+
+    beforeEach(()=>{
+        /*
+        Use the created "NavigateToURL" class instance ("navigate" on line 7 above) to call "navigateToURL" function from NavigateToURL class 
+        in the "1-navigatetoURL.page.js" file, pass in "/" to navigate to "https://www.strategyconnect.co/" page:
+        */
+       navigate.navigateToURL('/');
+
+       //Verify if the URL after navigation is correct:
+       cy.url().should('eq', data.homePageURL);
+
+       //Verify if the browser tab tible contains text 'Access Top 1% Experts - Drive Real Business Impact | StrategyConnect':
+       cy.title().should('eq', data.tabTitle);
+
+       //Verify if logo is available on the home page:
+       cy.get(headerSection.logo).should('be.visible');
+       
+       //Verify some text on the home page;
+       cy.get(homepage.precisionTextLocator).should('have.text', data.titleHomePage);
+       cy.get('.w-36').should('have.text', 'Get Started');
+    });
+
+    it('allows to Know More about "Technology Due Diligence".', () => {
+        //Click the "KnowMore" button on "Technology Due Diligence" section on Home page
+        homepage.clickKnowMoreTechnologyDueDiligence();
+        //Verify if the Strategy Service page got launched
+        cy.get(strategyService.title).should('have.text', data.titleStrategyService);
+        //Click the "KnowMore" button on "Technology Due Diligence" section on Strategy Service
+        strategyService.clickKnowMoreTechnologyDueDiligence();
+        //Verify if the Technical Due Diligence Page has been launched
+        cy.url().should('eq', data.homePageURL + data.URLtechnicalDueDiligencePage);
+        cy.get(technicalDueDiligence.objectiveLocator).should('be.visible');
+    });
+});
